165fa698e9ab19573747ed53aff4398189210a6e
L3to <lf2006@hotmail.com.br>
2024-03-15 23:31:50 -0300

    correçao nos tchurns

--- EX6-2.py
+++ EX6-2.py
@@ -1,7 +1,7 @@
 import math
 numa = int(input("Um número? "))
 raiz = math.sqrt (numa)
-if raiz == int:
-    print("A raiz quadrada é " ,raiz)
+if raiz == int(raiz):
+    print(f"A raiz quadrada é {raiz:.0f}")
 else:
     print ("O número não possui raiz")
---
+++ parouimar.py
@@ -0,0 +1,8 @@
+num = int(input("Informe um numero: "))
+
+resto = num%2
+#se o resto for igual a 0 entao imprime que é par
+if resto== 0:
+    print(f'{num} é par')
+else:
+    print(f'{num} é ímpar')
