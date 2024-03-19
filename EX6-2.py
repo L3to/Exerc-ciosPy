497534f8b407b00903b8bfc01ee59578c58e6d2e
L3to <lf2006@hotmail.com.br>
2024-03-19 19:36:41 -0300

    correçao exercicios 8 e 6

--- EX6-2.py
+++ EX6-2.py
@@ -1,7 +1,7 @@
 import math
 numa = int(input("Um número? "))
 raiz = math.sqrt (numa)
-if raiz == int(raiz):
-    print(f"A raiz quadrada é {raiz:.0f}")
+if raiz < 0 :
+    print ("Impossível extrair")
 else:
-    print ("O número não possui raiz")
+    print(f"A raiz quadrada é {raiz}")
--- EX8-2.py
+++ EX8-2.py
@@ -3,7 +3,7 @@ num2 = int(input("Segundo? "))
 soma = num1 + num2
 multi = num1 * num2
 
-if num1 == 0 or num2 == 0:
+if num2 == 0:
     print("A soma dos números",num1, "e", num2, "é dada por", soma,",a multplicação dá", multi,",já a divisão não daria certo porque um dos operadores é igual à 0, consequentemente não é possível ter um resto")
 else:
     divisao = num1/num2
